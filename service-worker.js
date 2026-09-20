ee13d1115c76ff2c293fd0da082becad2bf15593
Quentin Delisle <quentin.delisle@gmail.com>
2026-09-20 21:28:36 +0200

    go

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // TirScore — service worker : mise en cache de l'app pour un fonctionnement
 // hors-ligne une fois la page chargée une première fois.
-var CACHE = 'tirscore-v11';
+var CACHE = 'tirscore-v12';
 var ASSETS = [
   './',
   './index.html',
